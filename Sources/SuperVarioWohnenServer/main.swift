209a3d10991983360ed798ac3bb3b9d57c35c2c2
tobias <thinkdifferent055@gmail.com>
2017-11-01 15:48:39 +0100

    Setup kitura framework and ssl on linux

--- Sources/SuperVarioWohnenServer/main.swift
+++ Sources/SuperVarioWohnenServer/main.swift
@@ -1,1 +1,24 @@
-print("Hello, world!")
+import Kitura
+
+let router = Router()
+
+router.get("/") {
+    request, response, next in
+    response.send("Hello World")
+    next()
+}
+
+#if os(Linux)
+    let path = CommandLine.arguments[0]
+    let certFile = "\(path)/cert.pem"
+    let keyFile = "\(path)/privkey.pem"
+    let sslConfig = SSLConfig(withCACertificateDirectory: nil,
+                          usingCertificateFile: certFile,
+                          withKeyFile: keyFile,
+                          usingSelfSignedCerts: true)
+    Kitura.addHTTPServer(onPort: 2530, with: router, withSSL: sslConfig, keepAlive: .unlimited)
+#else
+    Kitura.addHTTPServer(onPort: 2530, with: router)
+#endif
+
+Kitura.run()
